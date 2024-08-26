11595c82d410a741a5f8c9d1318a043031d5253a
Grigory Efimov <grigory.efimov@gmail.com>
2024-08-27 02:17:46 +0300

    manifests/monitoring.pp added consul_meta

--- manifests/monitoring.pp
+++ manifests/monitoring.pp
@@ -1,8 +1,12 @@
 class collectd::monitoring (
   Boolean $consul,
+  Hash $consul_meta = {},
 ) {
   if $consul {
-    tools::consul_cfg { $module_name: port => 9103 }
+    tools::consul_cfg { $module_name:
+      port => 9103,
+      meta => $consul_meta,
+    }
     tools::consul_alert { $module_name:
       cfg => {
         alert => "$fqdn InstanceDown",
